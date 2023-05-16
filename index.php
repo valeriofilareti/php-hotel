fc861ed09c9184dc019aefb2339b05c28164484a
valeriofilareti <valerio.filareti@gmail.com>
2023-05-16 14:07:19 +0200

    [print] data

--- index.php
+++ index.php
@@ -46,10 +46,21 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
-  <ul>
-    <li><?php echo $hotels[0]['name'] ?></li>
-  </ul>
+  <div class="container-lg">
+    <ul>
+      <?php foreach($hotels as $hotel) : ?>
+        <div class="col mb-4">
+          <?php foreach($hotel as $date) : ?>
+            <li><?php echo $date?></li>
+          <?php endforeach ?>
+        </div>
+      <?php endforeach ?>
+    </ul>
+
+    
+  </div>
 </body>
 </html>
